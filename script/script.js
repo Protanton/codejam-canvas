7f7a57d809ff1eea58c6355a2052bbcc2c0c70a7
Protanton <protanton@gmail.com>
2019-10-27 23:41:29 +0300

    feat:add server request and pictures drawing

--- script/script.js
+++ script/script.js
@@ -20,3 +20,65 @@ const CANVAS_SIZE = 512;
   
     reDraw(canvas, MEDIUM_URL, DEFAULT_SIZE);
 })();
+
+function changeSize(elem, sizeItems, canvas, event) {
+    const newSize = parseInt(event.target.id);
+    if (elem.classList.contains('active')) return;
+    else {
+      sizeItems.forEach(element => {
+        if (element.classList.contains('active')) element.classList.remove('active');
+      })
+      elem.classList.add('active');
+    }
+    switch (newSize) {
+      case parseInt(document.querySelector('.small').id):
+        reDraw(canvas, SMALL_URL, newSize);
+        break;
+      case parseInt(document.querySelector('.medium').id):
+        reDraw(canvas, MEDIUM_URL, newSize);
+        break;
+      case parseInt(document.querySelector('.large').id):
+        reDraw(canvas, LARGE_URL, newSize);
+        break;
+      default:
+        console.log('error');
+    }
+}
+
+function reDraw(canvas, url, newSize) {
+    let ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+    if (url === LARGE_URL) {
+      ctx.fillStyle = '#fff';
+      ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+      const IMAGE = new Image();
+      fetch(url)
+        .then(response => response.blob())
+        .then(data => URL.createObjectURL(data))
+        .then(imageSrc => {
+          IMAGE.src = imageSrc
+          return IMAGE;
+        })
+        .then(image => {
+          image.onload = () => {
+            ctx.drawImage(image, CANVAS_SIZE / 4, CANVAS_SIZE / 4);
+          }
+        });
+    } else {
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          data.forEach((elem, i) => {
+            elem.forEach((color, j) => {
+              if (url === SMALL_URL) {
+                ctx.fillStyle = `#${color}`;
+              } else {
+                ctx.fillStyle = `rgba(${data[i][j]})`;
+              }
+              ctx.fillRect(i * (CANVAS_SIZE / newSize), j * (CANVAS_SIZE / newSize), (CANVAS_SIZE / newSize), (CANVAS_SIZE / newSize));
+            });
+          });
+        });
+    }
+}
